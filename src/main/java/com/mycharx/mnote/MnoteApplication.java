d8fdf39d9f59044d46badd1ec9cdaacef1225fe4
zhangbokang <540064991@qq.com>
2018-09-01 11:59:37 +0800

    创建初始化项目。

---
+++ src/main/java/com/mycharx/mnote/MnoteApplication.java
@@ -0,0 +1,12 @@
+package com.mycharx.mnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MnoteApplication.class, args);
+    }
+}
