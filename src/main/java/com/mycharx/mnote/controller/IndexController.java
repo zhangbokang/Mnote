dab93d8843853743d2382e23b0810684d52863b6
zhangbokang <540064991@qq.com>
2018-09-15 13:44:58 +0800

    去除首页的自动跳转。

--- src/main/java/com/mycharx/mnote/controller/IndexController.java
+++ src/main/java/com/mycharx/mnote/controller/IndexController.java
@@ -18,6 +18,6 @@ public class IndexController {
      */
     @RequestMapping("")
     public String index() {
-        return "redirect:/schedule/schedulelist.html";
+        return "Hello Word!";
     }
 }
